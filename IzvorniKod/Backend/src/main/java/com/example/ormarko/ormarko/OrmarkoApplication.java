c888c1d77d6d0a7a674e1f3429bb333495227f34
lovroDuranec <ld54848@fer.hr>
2024-10-31 22:59:34 +0100

    Backend code initialization.

---
+++ IzvorniKod/Backend/src/main/java/com/example/ormarko/ormarko/OrmarkoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ormarko.ormarko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmarkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmarkoApplication.class, args);
+	}
+
+}
---
+++ IzvorniKod/Backend/src/test/java/com/example/ormarko/ormarko/OrmarkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ormarko.ormarko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmarkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
