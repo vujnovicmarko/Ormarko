e53f4d9ac5d2041ad3eae09507d0dc2e1401ca93
lbulic-FER <luka.bulic@fer.hr>
2024-11-02 18:43:50 +0100

    Relacijska shema baze podataka - UPDATE
    Uklanjanje enum struktura i povezanih atributnih ograničenja (zamjena s VARCHAR(20) tipom) radi lakše kompatibilnosti s poslužiteljem.

--- IzvorniKod/Baza podataka/create_schema.sql
+++ IzvorniKod/Baza podataka/create_schema.sql
@@ -1,10 +1,3 @@
-CREATE TYPE LOCATION_TYPE AS ENUM ('polica', 'ladica', 'šipka za odjecu');
-CREATE TYPE ARTICLE_CATEGORY AS ENUM ('majica', 'košulja', 'trenirka - gornji dio', 'trenirka - donji dio', 'traperice', 'cipele', 'tenisice', 'čizme', 'štikle', 'haljina', 'suknja', 'jakna', 'kaput');
-CREATE TYPE ARTICLE_SEASON AS ENUM ('proljeće', 'ljeto', 'jesen', 'zima');
-CREATE TYPE ARTICLE_OPEN AS ENUM ('otvoreno', 'zatvoreno', 'kiša/snijeg');
-CREATE TYPE ARTICLE_CASUAL AS ENUM ('za doma', 'sportsko', 'ležerno', 'radno', 'svečano');
-CREATE TYPE ARTICLE_COLOR AS ENUM ('bijela', 'siva', 'crna', 'crvena', 'plava', 'žuta', 'zelena', 'ljubičasta', 'narančasta', 'smeđa', 'roza', 'bež');
-
 CREATE TABLE users (
 	username			VARCHAR(20) PRIMARY KEY,
 	pass				VARCHAR(20) NOT NULL,
@@ -29,7 +22,7 @@ CREATE TABLE closets (
 CREATE TABLE locations (
 	location_id			SERIAL PRIMARY KEY,
 	closet_id			INT NOT NULL,
-	type_loc			LOCATION_TYPE NOT NULL,
+	location_type		VARCHAR(20) NOT NULL,
 	FOREIGN KEY (closet_id) REFERENCES closets(closet_id) ON DELETE CASCADE
 );
 
@@ -39,19 +32,12 @@ CREATE TABLE articles_user (
 	sharing				BOOL DEFAULT FALSE,
 	title				VARCHAR(50) NOT NULL,
 	image				BYTEA NOT NULL,
-	category			ARTICLE_CATEGORY NOT NULL,
-	season				ARTICLE_SEASON NOT NULL,
-	openess				ARTICLE_OPEN 
-							CONSTRAINT ch_category CHECK (
-							((category = 'cipele' OR category = 'tenisice' OR category = 'čizme' OR category = 'štikle') AND openess IS NOT NULL) OR
-							((category <> 'cipele' AND category <>'tenisice' AND category <> 'čizme' AND category <> 'štikle') AND openess IS NULL)
-							)
-							CONSTRAINT ch_season CHECK (
-							((season = 'zima' AND openess = 'zatvoreno') OR (season = 'zima' AND openess = 'kiša/snijeg')) OR
-							(season <> 'zima')),
-	how_casual			ARTICLE_CASUAL NOT NULL,
-	main_color			ARTICLE_COLOR NOT NULL,
-	side_color			ARTICLE_COLOR NOT NULL,
+	category			VARCHAR(20) NOT NULL,
+	season				VARCHAR(20) NOT NULL,
+	openess				VARCHAR(20),
+	how_casual			VARCHAR(20) NOT NULL,
+	main_color			VARCHAR(20) NOT NULL,
+	side_color			VARCHAR(20) NOT NULL,
 	description			VARCHAR(500),
 	FOREIGN KEY (location_id) REFERENCES locations(location_id) ON DELETE CASCADE
 );
@@ -60,7 +46,7 @@ CREATE TABLE articles_marketing (
 	article_id			SERIAL PRIMARY KEY,
 	article_marketer	VARCHAR(20) NOT NULL,
 	title				VARCHAR(20) NOT NULL,
-	category			ARTICLE_CATEGORY NOT NULL,
+	category			VARCHAR(20) NOT NULL,
 	image				BYTEA NOT NULL,
 	price				REAL NOT NULL,
 	FOREIGN KEY (article_marketer) REFERENCES marketers(username) ON DELETE CASCADE
