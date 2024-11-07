8b096d1b99d17974e294d1d0495bd99f02c52917
lb2203 <benickilana@gmail.com>
2024-11-07 17:47:06 +0100

    Connected frontend and backend for login

--- IzvorniKod/Backend/src/main/java/com/example/ormarko/ormarko/Security/SecurityConfig.java
+++ IzvorniKod/Backend/src/main/java/com/example/ormarko/ormarko/Security/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -39,10 +44,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                //.cors(cors -> cors.configure(http))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(l -> l.defaultSuccessUrl("/profile",true).loginPage("/login").permitAll())
                 .authorizeHttpRequests(registry ->{
-                    registry.requestMatchers("/",  "home", "/advertiser/**", "/signup/**", "api/signup/user").permitAll();
+                    registry.requestMatchers("/",  "/home", "/advertiser/**", "/signup/**", "api/signup/user","/login").permitAll();
                     registry.requestMatchers("/user/**", "/profile").authenticated();
                     registry.anyRequest().authenticated();
                 })
@@ -53,6 +60,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
