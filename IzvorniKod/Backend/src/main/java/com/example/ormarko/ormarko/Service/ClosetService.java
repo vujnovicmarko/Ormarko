9ac661c8a91e960688a9a0b3236848f98c964295
AAndrocec <andrija.androcec@fer.unizg.hr>
2025-01-13 20:30:17 +0100

    add closet functionality

--- IzvorniKod/Backend/src/main/java/com/example/ormarko/ormarko/Service/ClosetService.java
+++ IzvorniKod/Backend/src/main/java/com/example/ormarko/ormarko/Service/ClosetService.java
@@ -21,4 +21,8 @@ public class ClosetService {
     public Closet findClosetById(Integer id) {
         return closetRepository.findClosetByClosetId(id);
     }
+
+    public Closet saveCloset(Closet closet) {
+        return closetRepository.save(closet);
+    }
 }
